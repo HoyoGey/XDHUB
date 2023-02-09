b02f421da5daa157ae859b7402b9ca7cc4549d14
HoyoGey <117149371+HoyoGey@users.noreply.github.com>
2023-02-09 19:27:29 +0600

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,5 +1,5 @@
-if game.PlaceID == 893973440 then
-
-elseif game.PlaceID == 621129760 then
-
+if game.PlaceID == 893973440 then -- Flee The Facility
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/XDHUB/main/Game/FTF.lua'))()
+elseif game.PlaceID == 621129760 then -- KAT
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/HoyoGey/XDHUB/main/Game/KAT.lua'))()
 end
